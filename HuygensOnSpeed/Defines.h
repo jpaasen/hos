5f41737688a3e973235a71711750b2bca9263145
Jon Petter Aasen <jpaasen@ifi.uio.no>
2013-05-03 11:44:23 +0200

    Added ifndef to all defines.

--- HuygensOnSpeed/Defines.h
+++ HuygensOnSpeed/Defines.h
@@ -1,12 +1,16 @@
 #ifndef _DEFINES_HEADER_
 #define _DEFINES_HEADER_
 
+#ifndef timestamp
 #define timestamp float
+#endif
 
-#ifdef _WIN32
+#ifndef uint
 #define uint unsigned int
 #endif
 
+#ifndef PI
 #define PI (3.1415926535897932384626433832795f)
+#endif
 
 #endif
